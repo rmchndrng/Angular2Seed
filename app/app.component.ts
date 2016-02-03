b090d5ff8e406a2bfb33179a14caed4e34ecce7b
Ramachandran G <g.rmchndrn@gmail.com>
2016-02-03 17:39:10 +0530

    Added a simple model and moved the template to a seperate file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 ﻿import {Component} from 'angular2/core';
 
+import {IHero} from './models/IHero';
+
 @Component({
     selector: 'my-app',
-    template:'<h1>My First Angular2 App</h1>'
+    templateUrl: 'app/templates/app.html'
 })
 
 export class AppComponent {
-    
+    public title = 'Tour of Heroes';
+    public hero: IHero = {
+        id: 1,
+        name: "Windstorm"
+    };
 }
